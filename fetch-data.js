c7343378ce1c8eafbbb031baa609555676978d9f
Khalil Hafz <khalilhafz@gmail.com>
2025-08-03 13:08:05 +0300

    setup

---
+++ fetch-data.js
@@ -0,0 +1,29 @@
+
+async function fetchUserData() {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+    const dataContainer = document.getElementById('api-data');
+    try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const users = await response.json();
+      dataContainer.innerHTML = ''
+  const userList = document.createElement('ul')
+  users.forEach(user => {
+    listItem = document.createElement('li');
+    listItem.textContent = user.name;
+    userList.appendChild(listItem);
+
+  }); 
+  dataContainer.appendChild(userList);
+  } catch (error) {
+    dataContainer.innerHTML = 'Failed to load user data.'
+  }
+
+
+
+
+}
+
+document.addEventListener('DOMContentLoaded', fetchUserData)
